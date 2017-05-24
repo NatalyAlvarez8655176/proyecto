a90ab0c69d81ff208a7f53c1359a6e0ba8bfe181
NatalyAlvarez8655176 <naty7sex@gmail.com>
2017-05-24 02:44:58 -0400

    roles

--- client/loginModule/loginForm.js
+++ client/loginModule/loginForm.js
@@ -2,5 +2,12 @@ Template.loginForm.events({
 	"click #registerbtn" : function(e){
 		e.preventDefault();
 		myTemplates.set("registerForm");
-	} 
+	},
+	"click #close" : function(e){
+		e.preventDefault();
+	},
+	"submit form" :function(e){
+		Meteor.loginWithPassword(e.target.email.value,e.target.password.value);
+		return false;
+	}
 });
